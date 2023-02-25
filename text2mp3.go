f04794ffc0398fe84ba9ebffa279ccc43d449fa9
Tetsuyuki Kobayashi <tetsu.koba@gmail.com>
2023-02-25 13:08:42 +0900

    Split functions

--- text2mp3.go
+++ text2mp3.go
@@ -1,57 +1,52 @@
-
-// Command quickstart generates an audio file with the content "Hello, World!".
 package main
 
 import (
-        "context"
-        "fmt"
-        "io/ioutil"
-        "log"
+	"context"
+	"log"
+	"os"
 
-        texttospeech "cloud.google.com/go/texttospeech/apiv1"
-        "cloud.google.com/go/texttospeech/apiv1/texttospeechpb"
+	texttospeech "cloud.google.com/go/texttospeech/apiv1"
+	"cloud.google.com/go/texttospeech/apiv1/texttospeechpb"
 )
 
-func main() {
-        // Instantiates a client.
-        ctx := context.Background()
+func text2mp3(ctx context.Context, input_filename, output_filename string) {
+	data, err := os.ReadFile(input_filename)
+	if err != nil {
+		log.Fatal(err)
+	}
+	client, err := texttospeech.NewClient(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer client.Close()
 
-        client, err := texttospeech.NewClient(ctx)
-        if err != nil {
-                log.Fatal(err)
-        }
-        defer client.Close()
+	req := texttospeechpb.SynthesizeSpeechRequest{
+		Input: &texttospeechpb.SynthesisInput{
+			InputSource: &texttospeechpb.SynthesisInput_Text{Text: string(data[:])},
+		},
+		Voice: &texttospeechpb.VoiceSelectionParams{
+			LanguageCode: "en-US",
+			SsmlGender:   texttospeechpb.SsmlVoiceGender_NEUTRAL,
+		},
+		AudioConfig: &texttospeechpb.AudioConfig{
+			AudioEncoding: texttospeechpb.AudioEncoding_MP3,
+		},
+	}
 
-        // Perform the text-to-speech request on the text input with the selected
-        // voice parameters and audio file type.
-        req := texttospeechpb.SynthesizeSpeechRequest{
-                // Set the text input to be synthesized.
-                Input: &texttospeechpb.SynthesisInput{
-                        InputSource: &texttospeechpb.SynthesisInput_Text{Text: "Hello, World!"},
-                },
-                // Build the voice request, select the language code ("en-US") and the SSML
-                // voice gender ("neutral").
-                Voice: &texttospeechpb.VoiceSelectionParams{
-                        LanguageCode: "en-US",
-                        SsmlGender:   texttospeechpb.SsmlVoiceGender_NEUTRAL,
-                },
-                // Select the type of audio file you want returned.
-                AudioConfig: &texttospeechpb.AudioConfig{
-                        AudioEncoding: texttospeechpb.AudioEncoding_MP3,
-                },
-        }
+	resp, err := client.SynthesizeSpeech(ctx, &req)
+	if err != nil {
+		log.Fatal(err)
+	}
 
-        resp, err := client.SynthesizeSpeech(ctx, &req)
-        if err != nil {
-                log.Fatal(err)
-        }
-
-        // The resp's AudioContent is binary.
-        filename := "output.mp3"
-        err = ioutil.WriteFile(filename, resp.AudioContent, 0644)
-        if err != nil {
-                log.Fatal(err)
-        }
-        fmt.Printf("Audio content written to file: %v\n", filename)
+	err = os.WriteFile(output_filename, resp.AudioContent, 0644)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
 
+func main() {
+	ctx := context.Background()
+	inputfile_name := "hello.txt"
+	output_filename := "hello.mp3"
+	text2mp3(ctx, inputfile_name, output_filename)
+}
